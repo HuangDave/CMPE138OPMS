213bb1a84d67bf68d727701db0d761f8c68c84f5
DAVID <huangd95@gmail.com>
2017-04-20 16:16:05 -0700

    added initial server file

---
+++ server.js
@@ -0,0 +1,24 @@
+
+const express = require('express')
+
+// initialize express app...
+const app = express()
+// process.env.PORT lets the port be set by Heroku...
+const port = process.env.PORT || 8080
+
+// configure express...
+app
+
+    // required for Heroku
+    .set('view engine', 'ejs')
+
+    // set routes...
+    //.use('/auth', authRoutes)
+
+    // listen to Heroku or local port...
+    .listen(port, function() {
+        console.log('Running on port: ' + port)
+    })
+
+// export for use in testing
+module.exports = app
